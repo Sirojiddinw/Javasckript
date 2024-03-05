2a040fbb0b974b745b3777010825658d0602d3f7
Sirojiddin <Sirojiddinyusupov55@gmail.com>
2024-03-06 00:08:46 +0500

    create html

---
+++ script.js
@@ -0,0 +1,21 @@
+var name = prompt ("Sizning ismingiz")
+console.log(" Siznging ismingiz "+ name);
+
+var age = prompt ("Sizning yoshingiz")
+console.log(" Sizning yoshingiz "+age);
+
+var num = +prompt ("25+25=?")
+console.log("misol1  25+25=50 sizning javob "+num);
+
+var num = +prompt ("100-50?")
+console.log("misol2 100-50=50 sizning javob "+num);
+
+var num = +prompt ("150*2=?")
+console.log("misol3 150*2=300 sizning javob "+num);
+
+var num = +prompt ("10/2?")
+console.log("misol4 10/2=5 sizning javob ");
+
+var num = +prompt ("13%3?")
+console.log("misol5 13%3=1 sizning javob ");
+
